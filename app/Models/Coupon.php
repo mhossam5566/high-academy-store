bbb5fd639965ef30c1bb1c00a48431f53339ad65
Mohamed Hossam <71794742+mhossam5566@users.noreply.github.com>
2026-01-29 00:53:31 +0200

    Update Coupon.php

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -2,31 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Coupon extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'name',
         'image',
         'price',
         'type'
-        ];
-        
-        public function vouchers(){
-            return $this->HasMany(Voucher::class);
+    ];
+
+    public function vouchers()
+    {
+        return $this->HasMany(Voucher::class);
+    }
+
+    public function getImagePathAttribute()
+    {
+        if ($this->image == null) {
+            return null;
+        } else {
+            return storage_path($this->image);
         }
-        
-        public function getImagePathAttribute()
-{
-   if($this->image == null){
-       return null;
-   }else{
-       return url($this->image);
-   }
-}
+    }
 
 }
