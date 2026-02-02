9447d869ff2e4afaa59b7844a9aa376e6853bed2
Mohamed Hossam <71794742+mhossam5566@users.noreply.github.com>
2026-02-03 00:31:08 +0200

    Update crud-helper.js

--- public/dashboard/assets/js/crud-helper.js
+++ public/dashboard/assets/js/crud-helper.js
@@ -26,6 +26,7 @@ window.CRUDHelper = {
                 ajax: this.config.ajaxUrl,
                 columns: this.config.columns,
                 responsive: false,
+                order: []
             });
         },
 
