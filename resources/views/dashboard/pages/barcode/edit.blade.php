92d7ac3838d2b4fca9246328269d886f66d795f6
Mohamed Hossam <71794742+mhossam5566@users.noreply.github.com>
2026-04-06 13:33:56 +0200

    Update edit.blade.php

--- resources/views/dashboard/pages/barcode/edit.blade.php
+++ resources/views/dashboard/pages/barcode/edit.blade.php
@@ -21,7 +21,8 @@
                 <a href="{{ route('dashboard.orders.details', $order->id) }}" class="btn btn-info">
                     <i class="ti ti-eye me-1"></i>تفاصيل الطلب
                 </a>
-                <a href="{{ route('dashboard.orders.barcode.list', ['shipping' => request('shipping')]) }}" class="btn btn-secondary">
+                <a href="{{ route('dashboard.orders.barcode.list', ['shipping' => request('shipping')]) }}"
+                    class="btn btn-secondary">
                     <i class="ti ti-arrow-right me-1"></i>العودة
                 </a>
             </div>
